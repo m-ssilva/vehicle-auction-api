6a1558a48d382483ab76fbcf1e1e37d51640262a
mssilva <bleed-it-out@live.com>
2021-01-13 15:23:49 -0300

    better log print format

--- server/logger.js
+++ server/logger.js
@@ -1,11 +1,12 @@
 const winston = require('winston')
-const { format: { combine, timestamp, prettyPrint } } = winston
+const { format: { combine, timestamp, json, colorize } } = winston
 
 const logger = winston.createLogger({
   level: 'info',
   format: combine(
     timestamp(),
-    prettyPrint()
+    json(),
+    colorize({ all: true })
   ),
   transports: [new winston.transports.Console()]
 })
